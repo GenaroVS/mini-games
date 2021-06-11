f4b2014877e8c754a91fabf6abab51c431497485
Genaro V Salinas <gvsalinas3@gmail.com>
2021-06-10 21:59:44 -0500

    Tile component, test one shows the bombs

---
+++ minesweeper/src/features/board/TileTest.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import './board.css';
+import { TileType } from './boardSlice';
+
+type propTypes = TileType & {
+  gameOver: boolean;
+  tilesNeeded: number;
+  row: number;
+  col: number;
+};
+
+const TileTest = ({
+  isBomb,
+  revealed,
+  flagged,
+  value,
+  gameOver,
+  tilesNeeded,
+  row,
+  col
+}: propTypes) => {
+
+  if (gameOver || tilesNeeded === 0) {
+    if (isBomb) {
+      return (
+        <div className='tile reveal' data-row={row} data-col={col}>
+          <i className='fas fa-skull-crossbones bad-icons'></i>
+        </div>
+      )
+    } else {
+      return (
+        <div className='tile reveal' data-row={row} data-col={col}>
+          {value === 0 ? '' : value}
+        </div>
+      )
+    }
+  } else if (isBomb) {
+    return (
+      <div className='tile bomb' data-row={row} data-col={col}>
+        { flagged &&  <i className='fas fa-flag bad-icons'></i> }
+      </div>
+    )
+  } else if (revealed) {
+    return (
+      <div className='tile reveal' data-row={row} data-col={col}>
+        {value === 0 ? '' : value}
+      </div>
+    )
+  }
+  return (
+    <div className='tile hidden' data-row={row} data-col={col}>
+      { flagged &&  <i className='fas fa-flag bad-icons'></i> }
+    </div>
+  )
+};
+
+export default TileTest;
+
+
